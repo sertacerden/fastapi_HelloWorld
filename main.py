f494597a69e8ed4dbbd3f12386191ef49a91125f
Sertaç <49095095+sertacerden@users.noreply.github.com>
2024-10-13 19:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from fastapi import Query
+from fastapi import Query, Body
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    age: int
 
 app = FastAPI()
 
 @app.get("/helloworld")
-def show_msg(name: str = Query("World", description="Enter your name to display your name. Otherwise the code will print Hello World")):
+def show_msg(
+    name: str = Query("World", description="Enter your name to display your name. Otherwise the code will print Hello World")):
     return {
-        "message": "Hello {}".format(name)
+        "message": "Hello {}".format(name),
     }
+async def create_item(item: Item):
+    return item
